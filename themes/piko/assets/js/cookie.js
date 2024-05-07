92dec1db23dd81f5163e100bbc76dc384fbf7a7d
Fabio Rueda <avances123@gmail.com>
2024-05-07 23:06:11 +0200

    consent

--- themes/piko/assets/js/cookie.js
+++ themes/piko/assets/js/cookie.js
@@ -1,24 +1,28 @@
-function getCookie(cname) {
-    const name = cname + "=";
-    const decodedCookie = decodeURIComponent(document.cookie);
-    const ca = decodedCookie.split(';');
-    for(let i = 0; i < ca.length; i++) {
-        let c = ca[i];
-        while (c.charAt(0) === ' ') {
-            c = c.substring(1);}
-        if (c.indexOf(name) === 0) {
-            return c.substring(name.length, c.length);}}
-    return "";}
-function setCookie(cname, cvalue, exdays) {
-    const d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    const expires = "expires="+ d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";}
-    const cookieBanner = document.querySelector('#cookie-banner');
-    const hasCookieConsent = getCookie('cookies-consent');
-if (!hasCookieConsent) {
-    cookieBanner.classList.remove('hidden');}
-    const consentCta = cookieBanner.querySelector('#consent-cookies');
-    consentCta.addEventListener('click', () => {
-    cookieBanner.classList.add('hidden');
-    setCookie('cookies-consent', 1, 365);});
+function hideBanner() {
+    document.getElementById('cookie-consent-banner').style.display = 'none';
+}
+
+if(localStorage.getItem('consentMode') === null) {
+    document.getElementById('btn-accept-all').addEventListener('click', function() {
+        setConsent({
+        necessary: true,
+        analytics: true,
+        preferences: true,
+        marketing: true
+        });
+        hideBanner();
+    });
+    document.getElementById('cookie-consent-banner').style.display = 'block';
+}
+
+function setConsent(consent) {
+    const consentMode = {
+        'functionality_storage': consent.necessary ? 'granted' : 'denied',
+        'security_storage': consent.necessary ? 'granted' : 'denied',
+        'ad_storage': consent.marketing ? 'granted' : 'denied',
+        'analytics_storage': consent.analytics ? 'granted' : 'denied',
+        'personalization': consent.preferences ? 'granted' : 'denied',
+    };
+    gtag('consent', 'update', consentMode);  
+    localStorage.setItem('consentMode', JSON.stringify(consentMode));
+}
